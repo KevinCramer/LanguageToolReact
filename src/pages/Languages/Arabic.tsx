b97ce55c3172117af61135054bc93c013bb538e3
KevinCramer <31591077+KevinCramer@users.noreply.github.com>
2022-12-03 15:58:02 +0000

    adding forms and quiz stuff

--- src/pages/Languages/Arabic.tsx
+++ src/pages/Languages/Arabic.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import {arabic as language} from "../../data/words"
 
+
 export function Arabic(){
     var x =1;
+    var setting = "Study"
     var [x,setX] = useState(x)
     const handleClick = () => { return setX(x*(-1))}
     var category = "Food"
@@ -10,7 +12,27 @@ export function Arabic(){
     const changeCategoryToFood = () => { return setCategory("Food")}
     const changeCategoryToColours = () => { return setCategory("Colours")}
     const changeCategoryToNumbers = () => { return setCategory("Numbers")}
+    var [setting,setSetting] = useState(setting)
+    const changeSetting = () => { return setSetting((setting === "Study" ? "Quiz": "Study"))}
+    function Arabic2( StudyOrQuiz: string){
+        if (StudyOrQuiz === "Study") {
+            return (
+   
+                <div>
+                    {category_words.map(word => (<div key={word.englishWord} > {x===1? word.englishWord + " = " + word.foreignWord : word.foreignWord + " = " + word.englishWord} </div>))}
+                </div>
+        )
+        }
+        if (StudyOrQuiz === "Quiz") {
+            return (
+                <div>
+                    {category_words.map(word => (<div><div key={word.englishWord} >{x===1? word.englishWord + " = " : word.foreignWord + " = "}  <form style={{ display: 'flex', flexDirection : 'row'}}><input type="text" id="name" /></form></div></div>))}
+                    
+                </div>
+            )
+        }
 
+    }
 
     var category_words = language.filter((word) => {return word.topic === category} )
 
@@ -21,11 +43,12 @@ export function Arabic(){
                 <button onClick={changeCategoryToFood} >Food</button>
                 <button onClick={changeCategoryToColours} >Colours</button>
                 <button onClick={changeCategoryToNumbers} >Numbers</button>
+                <button onClick={changeSetting} >Quiz/Study</button>
                 <p></p>
-                <div>
-                    {category_words.map(word => (<div key={word.englishWord} > {x===1? word.englishWord + " = " + word.foreignWord : word.foreignWord + " = " + word.englishWord} </div>))}
-                </div>
+                {Arabic2(setting)}
         </div>
         )
     }
+
+
     
