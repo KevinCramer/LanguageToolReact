b50a29a4fab5350ef33fd321f2d25e8bc4181712
KevinCramer <31591077+KevinCramer@users.noreply.github.com>
2022-12-03 13:40:28 +0000

    placing the vocab in a seperate file

---
+++ src/data/words.tsx
@@ -0,0 +1,16 @@
+export const data =[ 
+    {language: "Arabic", topic: "Food", englishWord: "potato", foreignWord: "batatis (بَطَاطِس)"},
+    {language: "Arabic", topic: "Food", englishWord: "bread", foreignWord: "khubz (خُبْز)"},
+    {language: "Arabic", topic: "Colours", englishWord: "yellow", foreignWord: "asfar (أَصْفَر)"},
+    {language: "Arabic", topic: "Colours", englishWord: "blue", foreignWord: "azraq (أَزْرَق)"},
+    {language: "Arabic", topic: "Numbers", englishWord: "two", foreignWord: "ithnain (اِثْنَان)"},
+    {language: "Arabic", topic: "Numbers", englishWord: "five", foreignWord: "khamsah (خَمْسَة)"},
+    {language: "Arabic", topic: "Numbers", englishWord: "one", foreignWord: "waheed (وَاحِد)"},
+    {language: "Dutch", topic: "Clothes", englishWord: "trousers", foreignWord: "broek"},
+    {language: "Dutch", topic: "Clothes", englishWord: "sweater", foreignWord: "trui"},
+    {language: "Dutch", topic: "Colours", englishWord: "red", foreignWord: "rood"},
+    {language: "Dutch", topic: "Colours", englishWord: "blue", foreignWord: "blauw"},
+    {language: "Dutch", topic: "Numbers", englishWord: "six", foreignWord: "zes"},
+    {language: "Dutch", topic: "Numbers", englishWord: "eight", foreignWord: "acht"},
+    {language: "Dutch", topic: "Numbers", englishWord: "eleven", foreignWord: "elf"}
+    ]
