379de631250449ad463a8358d2d82113a9b421fe
KevinCramer <31591077+KevinCramer@users.noreply.github.com>
2022-12-07 16:19:16 +0000

    git push origin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,19 @@
 import {Nav, Container, Navbar as NavbarBs} from "react-bootstrap"
 import {NavLink} from "react-router-dom"
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 export function Navbar(){
     return(
             <NavbarBs className = "bg-white shadow-sm mb-3">
                 <Container>
                     <Nav.Link to ="/" as= {NavLink}>Home</Nav.Link>
                     <Nav.Link to ="/about" as= {NavLink}>About</Nav.Link>
-                    <NavbarBs className = "bg-white  mb-1">
-                        <Nav.Link to ="/study/arabic" as= {NavLink}><label style= {{width: "100px"}}>Arabic</label></Nav.Link>
-                        <Nav.Link to ="/study/dutch" as= {NavLink}><label style= {{width: "100px"}}>Dutch</label></Nav.Link>
-                        <Nav.Link to ="/study/spanish" as= {NavLink}><label style= {{width: "100px"}}>Spanish</label></Nav.Link>
-                    </NavbarBs>
+                    <DropdownButton id="Languages" title="Languages">
+                        <Dropdown.Item href="/study/arabic"><Nav.Link to ="/study/arabic" as= {NavLink}>Arabic</Nav.Link></Dropdown.Item>
+                        <Dropdown.Item href="/study/dutch"><Nav.Link to ="/study/dutch" as= {NavLink}>Dutch</Nav.Link></Dropdown.Item>
+                        <Dropdown.Item><Nav.Link to ="/study/spanish" as= {NavLink}>Spanish</Nav.Link></Dropdown.Item>
+                    </DropdownButton>
                 </Container>
             </NavbarBs>
     )
