36241640791c832e530c66087ea0b3381e064d15
KevinCramer <31591077+KevinCramer@users.noreply.github.com>
2022-12-10 14:40:59 +0000

    clean refactored code

--- src/components/StudyContent.tsx
+++ src/components/StudyContent.tsx
@@ -4,30 +4,29 @@ import StudyElement from "./StudyElement"
 import {Container, Navbar as NavbarBs, Nav} from "react-bootstrap"
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import {languages, arabic, arabic_topics, dutch, dutch_topics, spanish, spanish_topics} from "../data/words"
+import {languages} from "../data/words"
 
 
 
 const StudyContent = (props: any) => {
-    var currentLanguage = arabic  
-    var currentLanguageName = "Arabic"
-    var [currentLanguage,setLanguage] = useState(currentLanguage)
+    var currentLanguageName = languages[0].languageName
     var [currentLanguageName,setLanguageName] = useState(currentLanguageName)
-    var currentLanguageTopics = arabic_topics
+
+    var currentLanguage = languages[0].Content
+    var [currentLanguage,setLanguage] = useState(currentLanguage)
+
+    var currentLanguageTopics = languages[0].topics
     var [currentLanguageTopics,setCurrentLanguageTopics] = useState(currentLanguageTopics)
+
     var current_topic = currentLanguageTopics[0]
     var [current_topic,setCurrentTopic] = useState(current_topic)
-    const changeCurrentLanguageToArabic = () => { return setLanguage(arabic),setLanguageName("Arabic"),setCurrentLanguageTopics(arabic_topics)}
-    const changeCurrentLanguageToDutch = () => { return setLanguage(dutch),setLanguageName("Dutch"),setCurrentLanguageTopics(dutch_topics)}
-    const changeCurrentLanguageToSpanish = () => { return setLanguage(spanish),setLanguageName("Spanish"),setCurrentLanguageTopics(spanish_topics)}
-    const changeCurrentLanguageToX= (languageName:string, languageContent:any) => { return setLanguage(languageContent),setLanguageName(languageName)}
 
-    
     var showBaseLanguage = true;
     var [showBaseLanguage,setShowBaseLanguage] = useState(showBaseLanguage)
-    const handleClick = () => { return setShowBaseLanguage(!showBaseLanguage)}
 
-    var changeCurrentTopicToX = (topic:string) => { return setCurrentTopic(topic)}
+    const handleClick = () => { return setShowBaseLanguage(!showBaseLanguage)}
+    const changeCurrentLanguage= (languageName:string, languageContent:any, topics: any) => { return setLanguage(languageContent),setLanguageName(languageName), setCurrentLanguageTopics(topics)}
+    const changeCurrentTopic = (topic:string) => { return setCurrentTopic(topic)}
 
     var quiz = false
     var [quiz,setQuiz] = useState(quiz)
@@ -66,13 +65,12 @@ const StudyContent = (props: any) => {
             <NavbarBs className = "bg-white shadow-sm mb-3">
                 <Container>
                     <DropdownButton id="Languages" title={"Language: " + String(currentLanguageName)}> 
-                        <Dropdown.Item onClick = {changeCurrentLanguageToArabic}>Arabic</Dropdown.Item>
-                        <Dropdown.Item onClick = {changeCurrentLanguageToDutch}>Dutch</Dropdown.Item>
-                        <Dropdown.Item onClick = {changeCurrentLanguageToSpanish}>Spanish</Dropdown.Item>
+                        {languages.map((languageItem: any) =>
+                        <Dropdown.Item onClick = {() => changeCurrentLanguage(languageItem.languageName, languageItem.Content,languageItem.topics)}>{languageItem.languageName}</Dropdown.Item>)}
                     </DropdownButton>
                     <DropdownButton id="Topics" title={"Topic: " + current_topic}>
                         {currentLanguageTopics.map((topic: string) =>
-                        <Dropdown.Item onClick = {() => changeCurrentTopicToX(topic)}>{topic}</Dropdown.Item>)}
+                        <Dropdown.Item onClick = {() => changeCurrentTopic(topic)}>{topic}</Dropdown.Item>)}
                     </DropdownButton>
                     <DropdownButton id="Settings" title="Learning Parameters">
                         <Dropdown.Item onClick = {handleClick}>Toggle base language</Dropdown.Item>
