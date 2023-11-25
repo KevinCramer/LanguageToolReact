c94b87f74876b0bb1924b7d12e956a8bc35a790c
Kevin Cramer <kevincramer26@gmail.com>
2023-11-25 13:16:58 +0000

    last push

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
-import {Nav, Container, Navbar as NavbarBs} from "react-bootstrap"
-import {NavLink} from "react-router-dom"
+import {Nav, Container, Navbar as NavbarBs} from 'react-bootstrap'
+import {NavLink} from 'react-router-dom'
 
 export function Navbar(){
   return(
-    <NavbarBs style = {{color: "white"}} className = "bg-black shadow-sm">
+    <NavbarBs style = {{color: 'white'}} className = "bg-black shadow-sm">
       <Container>
         <Nav.Link to ="/" as= {NavLink}>Home</Nav.Link>
         <Nav.Link to ="/study" as= {NavLink}>Study</Nav.Link>
