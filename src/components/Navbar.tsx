3035e9a02bb0e1bc71f3dec8fde4c0a26263f757
Kevin Cramer <kevincramer26@gmail.com>
2023-12-10 12:57:54 +0000

    adding better navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import {Nav, Container, Navbar as NavbarBs} from 'react-bootstrap'
 import {NavLink} from 'react-router-dom'
-
 export function Navbar(){
   return(
-    <NavbarBs>
+    <NavbarBs expand="lg" className="bg-body-tertiary" >
       <Container style={{justifyContent: 'center'}}>
-        <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/" as= {NavLink}>Home</Nav.Link>
-        <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/study" as= {NavLink}>Study</Nav.Link>
-        <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/about" as= {NavLink}>About</Nav.Link>
+        <NavbarBs.Toggle aria-controls="basic-navbar-nav" />
+        <NavbarBs.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/" as= {NavLink}>Home</Nav.Link>
+            <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/study" as= {NavLink}>Study</Nav.Link>
+            <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/about" as= {NavLink}>About</Nav.Link>
+          </Nav>
+        </NavbarBs.Collapse >
       </Container>
     </NavbarBs>
   )
