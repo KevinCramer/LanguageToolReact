3542475537b7c110febe23a46d0ad4feaa9a93a1
Kevin Cramer <kevincramer26@gmail.com>
2023-12-10 13:06:15 +0000

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,11 @@ export function Navbar(){
   return(
     <NavbarBs expand="lg" className="bg-body-tertiary" >
       <Container style={{justifyContent: 'center'}}>
-        <NavbarBs.Toggle aria-controls="basic-navbar-nav" />
-        <NavbarBs.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/" as= {NavLink}>Home</Nav.Link>
-            <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/study" as= {NavLink}>Study</Nav.Link>
-            <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/about" as= {NavLink}>About</Nav.Link>
-          </Nav>
-        </NavbarBs.Collapse >
+        <Nav className="me-auto">
+          <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/" as= {NavLink}>Home</Nav.Link>
+          <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/study" as= {NavLink}>Study</Nav.Link>
+          <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/about" as= {NavLink}>About</Nav.Link>
+        </Nav>
       </Container>
     </NavbarBs>
   )
