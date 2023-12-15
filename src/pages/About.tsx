d5c439c66b369ff83ab8d4b583ffa2417cc6d97f
Kevin Cramer <kevincramer26@gmail.com>
2023-12-15 15:20:45 +0000

    fix: improving text formatting on about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,13 +5,11 @@ export function About(){
   return (
     <div style={{fontSize: '2vh'}} className = "content2">
       <p > 
-            LingoCommand gives you what you need to create your own tailor made language learning experiences.
-
-      </p>
-      <p>
-            Under the 'Study' page you can select a wide variety of languages and topics. For a given language and topic you can then
-            create custom learning experiences by experimenting with the 'learning parameters'. For example press the Revise/Quiz 
-            parameter to gain access to the quiz which will test your knowledge
+        LingoCommand gives you what you need to create your own tailor made language learning experiences.<br/>
+        Under the 'Study' page you can select a wide variety of languages and topics.<br/> 
+        For a given language and topic you can thencreate custom learning experiences<br/>
+        by experimenting with the 'learning parameters'. For example press the Revise/Quiz<br/>
+        parameter to gain access to the quiz which will test your knowledge<br/>
       </p>
       <h3 style={{fontSize: '3vh'}}>
             Developed by Kevin Cramer
