87ce0412ef17f3443425a4b1c67b7432e7ddfc89
Kevin Cramer <kevincramer26@gmail.com>
2023-12-15 16:00:53 +0000

    refactor: putting logo.png under assets folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import './text.css'
-import logo from './logo.png';
+import logo from '../assets/logo.png';
 
 export function Home(){
   return <div>
     <div className = "imageContent">
-      <img src ={logo} width={120} height={100}/>
+      <img src ={logo} width={120} height={100} alt="logo"/>
     </div>
     <div className = "content2">
       <h1> 
