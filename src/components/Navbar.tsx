91974858b8e1b865f60eb896f3e3e780d12a143c
Kevin Cramer <kevincramer26@gmail.com>
2023-12-27 14:28:13 +0000

    bringing navbar spacing down

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,12 @@ export function Navbar(){
     <NavbarBs className="bg-body-tertiary" >
       <Container style={{justifyContent: 'center'}}>
         <Nav>
-          <Nav.Link style={{margin: '0px 20px 0px 20px'}} to ="/" as= {NavLink}>Home</Nav.Link>
-          <Nav.Link style={{margin: '0px 20px 0px 20px'}} 
+          <Nav.Link style={{margin: '0px 10px 0px 10px'}} to ="/" as= {NavLink}>Home</Nav.Link>
+          <Nav.Link style={{margin: '0px 10px 0px 10px'}} 
             to ="/vocabulary" as= {NavLink}>Vocabulary</Nav.Link>
-          <Nav.Link style={{margin: '0px 20px 0px 20px'}} 
+          <Nav.Link style={{margin: '0px 10px 0px 10px'}} 
             to ="/grammar" as= {NavLink}>Grammar</Nav.Link>
-          <Nav.Link style={{margin: '0px 20px 0px 20px'}} 
+          <Nav.Link style={{margin: '0px 10px 0px 10px'}} 
             to ="/about" as= {NavLink}>About</Nav.Link>
         </Nav>
       </Container>
