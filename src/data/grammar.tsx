fa3103da01d2e8152d1ab3d26aacc0f34b9a9ba9
KevinCramer <31591077+KevinCramer@users.noreply.github.com>
2023-12-30 16:30:15 +0100

    Update grammar.tsx

--- src/data/grammar.tsx
+++ src/data/grammar.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @stylistic/js/max-len */
 
 const spanishTopics: any[] = [
-  {name:'definite article', content: 'Masculine singular: el, Masculine plural: los, Feminine singular: la. Feminine plural: las'}, 
-  {name:'indefinite article', content: 'Masculine singular: un, Masculine plural: unos, Feminine singular: una. Feminine plural: unas'},
+  {name:'definite article', contents: [' The definite article specificies which object, person, or place is being talked about. English only has one definite article, namely the word "the". Spanish has 4 definite articles:', 'Masculine singular: el','Masculine plural: los', 'Feminine singular: la', 'Feminine plural: las', 'For example the word for hand in Spanish is mano. Mano is feminine in Spanish. Thus we say "la mano".']}, 
+  {name:'indefinite article', contents: [' The indefinite article is a non specific way to refer to a noun. English has two indefinite articles, namely "a" and "an". Spanish has 4 indefinite articles: ', 'Masculine singular: un', 'Masculine plural: unos', 'Feminine singular: una', 'Feminine plural: unas', 'For example the word for eye in Spanish is ojo. It is masculine in Spanish. Thus we say in Spanish "un ojo"']},
 ]
 
 export const languages: any[] = 
