5fe910934eade8c10e2e742d81581395f2dab92c
KevinCramer <31591077+KevinCramer@users.noreply.github.com>
2023-12-30 16:48:42 +0100

    Update grammar.tsx

--- src/data/grammar.tsx
+++ src/data/grammar.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @stylistic/js/max-len */
 
 const spanishTopics: any[] = [
-  {name:'definite article', contents: [' The definite article specificies which object, person, or place is being talked about. English only has one definite article, namely the word "the". Spanish has 4 definite articles:', 'Masculine singular: el','Masculine plural: los', 'Feminine singular: la', 'Feminine plural: las', 'For example the word for hand in Spanish is mano. Mano is feminine in Spanish. Thus we say "la mano".']}, 
-  {name:'indefinite article', contents: [' The indefinite article is a non specific way to refer to a noun. English has two indefinite articles, namely "a" and "an". Spanish has 4 indefinite articles: ', 'Masculine singular: un', 'Masculine plural: unos', 'Feminine singular: una', 'Feminine plural: unas', 'For example the word for eye in Spanish is ojo. It is masculine in Spanish. Thus we say in Spanish "un ojo"']},
+  {name:'definite article', contents: [' The definite article specificies which object, person, or place is being talked about. English only has one definite article, namely the word "the". For example we say "the car", or "the city" in English. Spanish on the other hand has 4 definite articles:','\0', '1) Masculine singular: el','2) Masculine plural: los', '3) Feminine singular: la', '4) Feminine plural: las','\0', 'For example the word for hand in Spanish is mano. Mano is a feminine noun in Spanish. Thus "the hand" is in Spanish "la mano".']}, 
+  {name:'indefinite article', contents: [' The indefinite article is a non specific way to refer to a noun. English has two indefinite articles, namely "a" and "an". Spanish has 4 indefinite articles: ','\0', '1) Masculine singular: un', '2) Masculine plural: unos', '3) Feminine singular: una', '4) Feminine plural: unas', '\0', 'For example the word for eye in Spanish is ojo. Ojo is a masculine noun in Spanish. Thus "an eye" is in Spanish "un ojo".']},
 ]
 
 export const languages: any[] = 
