44ce05a4b325d787b5e939b91610f3959b8d6f99
Kevin Cramer <kevincramer26@gmail.com>
2024-03-03 04:45:52 +0000

    feat: adding slug preservation for grammar section

--- src/components/molecules/GrammarContent.tsx
+++ src/components/molecules/GrammarContent.tsx
@@ -1,20 +1,44 @@
-import {useState } from 'react';
+import {useState, useEffect } from 'react';
 import {Container, Navbar as NavbarBs} from 'react-bootstrap';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import {languages} from '../../data/grammar';
+import { languageToSlugs} from '../../constants'
+import {queryParamCompress, queryParamDecompress} from '../../helpers/queryParamHelpers'
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const GrammarContent = () => {
-  var currentLanguage: any = languages[0]
+  const navigate = useNavigate();
+  var urlSearchParams = new URLSearchParams(useLocation().search);
+  const urlSettings = JSON.parse(queryParamDecompress(urlSearchParams.get('s') as string) as string) || []
+  const urlLanguage = urlSettings[0]
+  var currentLanguage: any = languages
+    .find(l => languageToSlugs[l.languageName] === urlLanguage) || languages[0]
   var [currentLanguage,setLanguage] = useState(currentLanguage);
 
-  var currentTopic = currentLanguage.topics[0]
+  const urlTopic = urlSettings[1]
+  var currentTopic = (currentLanguage.topics as any[])
+    .find(t => t.slugName === urlTopic) || currentLanguage.topics[0]
   var [currentTopic,setCurrentTopic] = useState(currentTopic)
 
   const changeCurrentLanguage = 
     ( language: any) => setLanguage(language);
   const changeCurrentTopic = (topic: any) => { return setCurrentTopic(topic);}
 
+  useEffect(() => {
+    const settings = [
+      languageToSlugs[currentLanguage.languageName],
+      currentTopic.slugName, 
+      true,
+      0, 
+      true,
+      false,
+      false
+    ]
+    navigate(`?s=${queryParamCompress(JSON.stringify(settings))}`);
+
+  }, [
+    currentLanguage.languageName, currentTopic.slugName, navigate ]);
   function ShowGrammarExplanation(){
     return (
       <div style={{ width: '400px'}}>
