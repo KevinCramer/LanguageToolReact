b6616aac7d0b8528530c0c033b2bd8fdf982f7e9
Kevin Cramer <kevincramer26@gmail.com>
2024-05-04 13:13:48 +0100

    chore: removing the hiding of grammar and immersion sections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,7 @@
 import { Container, Nav , Navbar as NavbarBs } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
-import { reduxStore } from '../../types/vocabTypes'
-import { useSelector } from 'react-redux';
 
 export function Navbar(){
-  const showInProgressFeatures = useSelector((state: reduxStore) => state.featureToggle.x);
   return(
     <NavbarBs className="bg-body-tertiary" >
       <Container style={{ justifyContent: 'center' }}>
@@ -12,17 +9,12 @@ export function Navbar(){
           <Nav.Link style={{ margin: '0px 10px 0px 10px' }} to ="/" as= {NavLink}>Home</Nav.Link>
           <Nav.Link style={{ margin: '0px 10px 0px 10px' }} 
             to ="/vocabulary" as= {NavLink}>Vocabulary</Nav.Link>
-          { showInProgressFeatures && (
-            <Nav.Link 
-              style={{ margin: '0px 10px 0px 10px' }} to="/grammar" as={NavLink}>
-    Grammar
-            </Nav.Link>
-          )}
-          { showInProgressFeatures && (
-            <Nav.Link style={{ margin: '0px 10px 0px 10px' }} to="/immersion" as={NavLink}>
-    Immersion
-            </Nav.Link>
-          )}
+          <Nav.Link style={{ margin: '0px 10px 0px 10px' }} to="/grammar" as={NavLink}> 
+          Grammar
+          </Nav.Link>
+          <Nav.Link style={{ margin: '0px 10px 0px 10px' }} to="/immersion" as={NavLink}>
+            Immersion 
+          </Nav.Link>
         </Nav>
       </Container>
     </NavbarBs>
