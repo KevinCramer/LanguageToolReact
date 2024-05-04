a0b37eeca68695e436088bc31b063ab71cb87ec2
Kevin Cramer <kevincramer26@gmail.com>
2024-05-04 13:33:05 +0100

    chore: hiding grammar again

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Container, Nav , Navbar as NavbarBs } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import { reduxStore } from '../../types/vocabTypes'
+import { useSelector } from 'react-redux';
 
 export function Navbar(){
+  const showInProgressFeatures = useSelector((state: reduxStore) => state.featureToggle.x);
   return(
     <NavbarBs className="bg-body-tertiary" >
       <Container style={{ justifyContent: 'center' }}>
@@ -9,11 +12,14 @@ export function Navbar(){
           <Nav.Link style={{ margin: '0px 10px 0px 10px' }} to ="/" as= {NavLink}>Home</Nav.Link>
           <Nav.Link style={{ margin: '0px 10px 0px 10px' }} 
             to ="/vocabulary" as= {NavLink}>Vocabulary</Nav.Link>
-          <Nav.Link style={{ margin: '0px 10px 0px 10px' }} to="/grammar" as={NavLink}> 
-          Grammar
-          </Nav.Link>
+          { showInProgressFeatures && (
+            <Nav.Link 
+              style={{ margin: '0px 10px 0px 10px' }} to="/grammar" as={NavLink}>
+    Grammar
+            </Nav.Link>
+          )}
           <Nav.Link style={{ margin: '0px 10px 0px 10px' }} to="/immersion" as={NavLink}>
-            Immersion 
+    Immersion
           </Nav.Link>
         </Nav>
       </Container>
