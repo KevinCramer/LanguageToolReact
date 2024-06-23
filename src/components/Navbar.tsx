811337fea74b4c0eaea84d1573c475bbeb8ad2ef
Kevin Cramer <kevincramer26@gmail.com>
2024-06-23 23:13:03 +0100

    fix: contact us with capital letters

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ export function Navbar() {
               } href="mailto:contact@lingocommand.com"
               onClick={closeNavbar} // Close navbar on click
             >
-    contact us
+            Contact Us
             </a>
           </Nav>
         
