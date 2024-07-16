df2ccb0aa18a685683b6fd6fe23f7cad10f3e3ff
Kevin Cramer <kevincramer26@gmail.com>
2024-07-16 17:41:32 +0100

    test: successfully adding unit tests using vitest which is way better than jest

---
+++ src/tests/unit-tests/vocab-content.helpers.spec.ts
@@ -0,0 +1,15 @@
+import { expect, it } from 'vitest'
+import { roughlyEqual } from '../../helpers/vocab-content-helpers';
+
+it(`GIVEN two string params that are equal ignoring case and whitespace
+THEN roughlyEqual returns true`, () => {
+  expect(roughlyEqual('  Hello ', 'hello')).toBe(true);
+  expect(roughlyEqual('WORLD', 'world')).toBe(true);
+});
+
+it(`GIVEN two string params that are not equal even when ignoring case and whitespace 
+THEN roughlyEqual returns false`, () => {
+  expect(roughlyEqual('Hello', 'world')).toBe(false);
+  expect(roughlyEqual('Test', 'Testing')).toBe(false);
+});
+
