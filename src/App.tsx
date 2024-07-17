2c3a2d4b58a1aac1a52b94dccbd904975fc47e7d
Kevin Cramer <kevincramer26@gmail.com>
2024-07-17 15:57:48 +0100

    feat adding a semi working sign up form with auth not checked

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.scss'
 import { Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import ContactUs from './pages/ContactUs/ContactUs'
 import GrammarContent from './pages/GrammarContent/GrammarContent'
 import Home from './pages/Home/Home'
 import ListeningComprehension from './pages/ListeningComprehension/ListeningComprehension'
 import Navbar from './components/atoms/Navbar/Navbar'
 import VocabContent from './pages/VocabContent/VocabContent'
+import Signup from './components/atoms/Signup/Signup'
 
 const App = ()=> {
   return (
-    <div className="full-background">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={ <div className='home-container'> <Home /></div>} />
-        <Route path="/vocabulary" element={<VocabContent />} />
-        <Route path="/grammar" element={<GrammarContent />} />
-        <Route path="/listening-comprehension" element={<ListeningComprehension />} />
-        <Route path="/contactus" element={<ContactUs />} />
+    <AuthProvider>
+      <div className="full-background">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={ <div className='home-container'> <Home /></div>} />
+          <Route path="/vocabulary" element={<VocabContent />} />
+          <Route path="/grammar" element={<GrammarContent />} />
+          <Route path="/listening-comprehension" element={<ListeningComprehension />} />
+          <Route path="/contactus" element={<ContactUs />} />
 
-      </Routes>
-    </div>
+        </Routes>
+      </div>
+      <Signup/>
+    </AuthProvider>
   );
 }
 
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { auth } from '../firebase'
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+
+// @ts-ignore
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+type AuthProviderProp = {
+  children: React.ReactNode;
+};
+
+export function AuthProvider({ children }: AuthProviderProp) {
+  const [currentUser, setCurrentUser] = useState()
+
+  function signup(email: any, password: any) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user: any) => {
+      setCurrentUser(user);
+    })
+
+    return unsubscribe
+  }, [])
+  
+  const value = {
+    currentUser,
+    signup,
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
