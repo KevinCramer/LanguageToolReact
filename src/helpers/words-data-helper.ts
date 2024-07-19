eb5e2b2c54a67e7bd9c93519e897253b0dc3ecc6
Kevin Cramer <kevincramer26@gmail.com>
2024-07-19 14:48:34 +0100

    feat: first sorting by islocked value. Then sorting by alphabetical order

--- src/helpers/words-data-helper.ts
+++ src/helpers/words-data-helper.ts
@@ -8,5 +8,10 @@ import {
 export const sortTopics = (
   topics: TopicWithOneAlphabet[] | TopicWithTwoAlphabets[] | TopicWithThreeAlphabets[]
 ) => {
-  return topics.sort((a:Topic,b: Topic) => a.name < b.name ? -1 : 1 )
+  return topics.sort((a: Topic, b: Topic) => {
+    if (a.isLocked === b.isLocked) {
+      return a.name < b.name ? -1 : 1;
+    }
+    return a.isLocked ? 1 : -1;
+  });
 }
