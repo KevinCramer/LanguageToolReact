3555d8f2a08c69abb98698a63c4771d69886d7c8
Kevin Cramer <kevincramer26@gmail.com>
2024-07-26 15:06:40 +0100

    feat: working checkout session in local development

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,21 @@
 import { displayDeleteAccount, displayUpdateProfile } from '../../redux-store/auth'
 import CustomLink from '../../components/atoms/CustomLink/CustomLink';
 import { useDispatch, } from 'react-redux';
+import { getCheckoutUrl } from '../../stripePayment';
+import app from '../../firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const getPremium = async () => {
+    const priceId = 'price_1PgT4NRraXayJTTJ54CIEfCQ';
+    const checkoutUrl = await getCheckoutUrl(app, priceId);
+    console.log('checkoutUrl: ', checkoutUrl)
+    window.location.href = checkoutUrl;
+    console.log('Upgrade to Premium');    
+  }
+
   return (
     <>
       <h4>Profile</h4>
@@ -12,6 +24,9 @@ const Profile = () => {
       </CustomLink>
       <CustomLink onClick={() => dispatch(displayDeleteAccount())}>
         Delete Account
+      </CustomLink>
+      <CustomLink onClick={() => getPremium()}>
+      Upgrade to Premium
       </CustomLink>
     </>
   );
