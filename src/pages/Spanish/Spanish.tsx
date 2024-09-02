ebafa5a2da4610974f549335ef88557570389cc9
Kevin Cramer <kevincramer26@gmail.com>
2024-09-02 14:16:35 +0100

    feat: adding upward trend icon

--- src/pages/Spanish/Spanish.tsx
+++ src/pages/Spanish/Spanish.tsx
@@ -2,6 +2,7 @@ import './Spanish.scss'
 import dart from '../../assets/dart.svg';
 import dictionary from '../../assets/dictionary.svg';
 import headphones from '../../assets/headphones.svg';
+import upwardTrend from '../../assets/upward-trend.svg';
 
 const Spanish = () => { 
  
@@ -9,13 +10,18 @@ const Spanish = () => {
     <>
       <div className="contact-us-container">
         <div>
-        The Spanish language has about 500 million native speakers, mainly in the Americas and Spain. Learning it opens doors to new connections, cultures, and job opportunities.
+        There are about 500 million native Spanish speakers, mainly in Spain and the Americas. Learning Spanish opens doors to new connections, cultures, and job opportunities.
         </div>
         <div>
+          <div>
+            <img src ={upwardTrend} width={100} height={100} alt="Grammar Icon"/>
+          </div>
           <a href="/spanish/study-guide">
           Study Guide 
           </a>
-          This guide helps beginners quickly learn Spanish and build confidence in speaking, whilst also offering plenty of valuable content for more advanced users.
+          <div>
+          This guide helps beginners learn Spanish and gain confidence in their skills. It also offers valuable content for advanced users.
+          </div>
         </div>
         Alternatively, you can create your own study plan using the resources available. At LingoCommand the Spanish study materials are divided into three main sections:
         <div>
