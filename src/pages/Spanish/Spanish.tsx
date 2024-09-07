71de1fd0d18a04be55819a96fec35d3607625e17
Kevin Cramer <kevincramer26@gmail.com>
2024-09-07 03:46:53 +0100

    fix: fixing formatting on spanish home page

--- src/pages/Spanish/Spanish.tsx
+++ src/pages/Spanish/Spanish.tsx
@@ -14,7 +14,7 @@ const Spanish = () => {
         </div>
         <div>
           
-          <div>
+          <div style={{ paddingTop:'30px' }}>
           This guide helps beginners learn Spanish and gain confidence in their skills. It also offers valuable content for advanced users.
           </div>
           <div>
@@ -24,28 +24,23 @@ const Spanish = () => {
           Study Guide 
           </a>
         </div>
+        <div style={{ paddingTop:'30px' }}>
         Alternatively, you can create your own study plan using the resources available. At LingoCommand the Spanish study materials are divided into three main sections:
-        <div>
-
         </div>
-        <div>
-          <img src ={dictionary} width={100} height={100} alt="Grammar Icon"/>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{ textAlign: 'center', margin: '10px' }}>
+            <img src={dictionary} width={100} height={100} alt="Grammar Icon" style={{ display: 'block', margin: '0 auto 10px' }} />
+            <a href="/spanish/vocabulary">Vocabulary</a>
+          </div>
+          <div style={{ textAlign: 'center', margin: '10px' }}>
+            <img src={dart} width={100} height={100} alt="Grammar Icon" style={{ display: 'block', margin: '0 auto 10px' }} />
+            <a href="/spanish/grammar/definite-article">Grammar</a>
+          </div>
+          <div style={{ textAlign: 'center', margin: '10px' }}>
+            <img src={headphones} width={100} height={100} alt="Listening Comprehension Icon" style={{ display: 'block', margin: '0 auto 10px' }} />
+            <a href="/spanish/listening-comprehension/me-gusta-leer">Listening <br />Comprehension</a>
+          </div>
         </div>
-        <a href="/spanish/vocabulary">
-          Vocabulary
-        </a>
-        <div>
-          <img src ={dart} width={100} height={100} alt="Grammar Icon"/>
-        </div>
-        <a href="/spanish/grammar/definite-article">
-          Grammar
-        </a>
-        <div>
-          <img src ={headphones} width={100} height={100} alt="Listening Comprehension Icon"/>
-        </div>
-        <a href="/spanish/listening-comprehension/me-gusta-leer">
-          Listening Comprehension
-        </a>
       </div>
     </>
   );
