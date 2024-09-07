16a678a7fddc3837c3b6416ea109677ca5c43fdb
Kevin Cramer <kevincramer26@gmail.com>
2024-09-07 03:36:49 +0100

    fix: making layout in spanish home page more consistent

--- src/pages/Spanish/Spanish.tsx
+++ src/pages/Spanish/Spanish.tsx
@@ -13,15 +13,16 @@ const Spanish = () => {
         There are about 500 million native Spanish speakers, mainly in Spain and the Americas. Learning Spanish opens doors to new connections, cultures, and job opportunities.
         </div>
         <div>
+          
+          <div>
+          This guide helps beginners learn Spanish and gain confidence in their skills. It also offers valuable content for advanced users.
+          </div>
           <div>
             <img src ={upwardTrend} width={100} height={100} alt="Grammar Icon"/>
           </div>
           <a href="/spanish/study-guide">
           Study Guide 
           </a>
-          <div>
-          This guide helps beginners learn Spanish and gain confidence in their skills. It also offers valuable content for advanced users.
-          </div>
         </div>
         Alternatively, you can create your own study plan using the resources available. At LingoCommand the Spanish study materials are divided into three main sections:
         <div>
