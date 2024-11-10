b333d352f61adfb8e37ea628ac3efa3553d455c3
Kevin Cramer <kevincramer26@gmail.com>
2024-11-10 22:40:12 +0000

    fix: fixing links on the Japanese Study Guide page

--- src/pages/Japanese/JapaneseStudyGuide.tsx
+++ src/pages/Japanese/JapaneseStudyGuide.tsx
@@ -8,12 +8,13 @@ const JapaneseStudyGuide = () => {
         <div className="central-container">
           <h4 style={{ paddingBottom: '30px' }}>Japanese Study Guide</h4>
           <div>
-            <b>Phase 1:</b> learn <a href="/japanese/vocabulary?s=hir-T0TFT">hiragana</a>,and <a href="/japanese/vocabulary?s=kat-T0TFT">katakana</a>. 
+            <b>Phase 1:</b> learn <a href="/japanese/writing-systems?s=hir-T0TFT">hiragana</a>,and <a href="/japanese/writing-systems?s=kat-T0TFT">katakana</a>. 
           </div>
           <div style={{ paddingTop:'20px' }}>
-            <b>Phase 2:</b> learn <a href="/japanese/vocabulary?s=kanji_grade1-T0TFT">kanji</a>.
+            <b>Phase 2:</b> learn <a href="/japanese/writing-systems?s=kanji_grade1-T0TFT">kanji</a>.
           </div>
-          <i style={{ fontSize:'16px', paddingTop: '30px', paddingBottom: '30px' }}>
+          <div style={{ height: '30px' }}></div>
+          <i style={{ fontSize:'16px', paddingBottom: '30px' }}>
           The Japanese language has 130 million native speakers, with about 95% residing in Japan. 
           Over the past 40 years, the number of people learning Japanese as a second language has increased over twentyfold. 
           </i>
