15ca8810d3d264ba0ee0ccf2fd7cbb5f2fab58a1
Kevin Cramer <kevincramer26@gmail.com>
2024-11-10 23:41:32 +0000

    feat: adding links to social media

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -11,6 +11,20 @@ const ContactUs = () => {
         <a href="mailto:contact@lingocommand.com" style={{ color: 'white' }}>
             contact@lingocommand.com
         </a>
+        <div style={{ color: 'white' , paddingTop: '50px' }}>
+          social media:
+        </div>
+        <div style={{ display:'flex', flexDirection: 'column', paddingTop: '5px', textAlign: 'center' }}>
+          <a href="https://www.youtube.com/@lingocommand" style={{ color: 'white' }}>
+            youtube
+          </a>
+          <a href="https://www.instagram.com/lingocommand/" style={{ color: 'white' }}>
+            instagram
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100068758832446" style={{ color: 'white' }}>
+            facebook
+          </a>
+        </div>
       </div>
       
     </>
