e5a2ab8a35eeb43d7892a8992695df59d2e9ef60
Kevin Cramer <kevincramer26@gmail.com>
2024-12-08 14:46:55 +0000

    protecting links for spanish content and improving formatting of Spanish Study guide

--- src/pages/Spanish/SpanishStudyGuide.tsx
+++ src/pages/Spanish/SpanishStudyGuide.tsx
@@ -1,21 +1,77 @@
+import { useProtectedLink } from '../../helpers/use-protected-link';
 import './Spanish.scss'
 
 const SpanishStudyGuide = () => { 
- 
+  const handleProtectedClick = useProtectedLink(); 
   return (
     <>
       <div className="page-container">
         <div className="central-container">
           <h4 style={{ paddingBottom: '30px' }}>Spanish Study Guide</h4>
           <div>
-            <b>Phase 1:</b> Study the vocabulary for these topics: <a href="/spanish/vocabulary?s=clo-T0TFT">clothes</a>, <a href="/spanish/vocabulary?s=num-T0TFT">numbers</a>, <a href="/spanish/vocabulary?s=col-T0TFT">colours</a>, <a href="/spanish/vocabulary?s=foo-T0TFT">food</a>, <a href="/spanish/vocabulary?s=hom-T0TFT">home</a>, <a href="/spanish/vocabulary?s=bod-T0TFT">body</a>, <a href="/spanish/vocabulary?s=loc-T0TFT">locations</a>. < br />We recommend you spend no more than 15 minutes studying a topic’s vocabulary before taking a quiz. Then if the grade is below 90% which is completely normal go back and do another round of 15 minutes of studying. Make sure to take breaks, and you can also switch between studying a few topics to mix things up. 
+            <b>Phase 1:</b> Study the vocabulary for these topics:
+            <ul>
+              <li>
+                <a href="/spanish/vocabulary?s=clo-T0TFT">clothes</a>
+              </li>
+              <li>
+                <a href="/spanish/vocabulary?s=num-T0TFT">numbers</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/vocabulary?s=foo-T0TFT">food</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/vocabulary?s=hom-T0TFT">home</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/vocabulary?s=bod-T0TFT">body</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/vocabulary?s=loc-T0TFT">locations</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/vocabulary?s=col-T0TFT">colours</a>
+              </li>
+            </ul>
+
+            < br />We recommend you spend no more than 15 minutes studying a topic’s vocabulary before taking a quiz. Then if the grade is below 90% which is completely normal go back and do another round of 15 minutes of studying. Make sure to take breaks, and you can also switch between studying a few topics to mix things up. 
           (If you get 90% or higher  for each quiz without studying the topic in the last 24 hours you are ready to move to phase 2). 
           </div>
           <div style={{ paddingTop:'20px' }}>
-            <b>Phase 2:</b> Study the vocabulary for these topics until you get 90% accuracy taking the quiz: <a href="/spanish/vocabulary?s=pro-T0TFT">pronouns</a>, <a href="/spanish/vocabulary?s=que-T0TFT">question words</a>, <a href="/spanish/vocabulary?s=con-T0TFT">conjunctions</a>. < br /> Use same approach as phase 1. 
+            <b>Phase 2:</b> Study the vocabulary for these topics until you get 90% accuracy taking the quiz: 
+            <ul>  
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/vocabulary?s=pro-T0TFT">pronouns</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/vocabulary?s=que-T0TFT">question words</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/vocabulary?s=con-T0TFT">conjunctions</a>
+              </li>
+            </ul>
+            < br /> Use same approach as phase 1. 
           </div>
           <div style={{ paddingTop:'20px' }}>
-            <b>Phase 3:</b> Study the first five <a href="/spanish/grammar/definite-article">grammar lessons</a>.   
+            <b>Phase 3:</b> Study the first five grammar lessons: 
+            <b>Phase 3:</b> Study the first five grammar lessons: 
+            <ol>
+              <li>
+                <a href="/spanish/grammar/definite-article">definite article</a>
+              </li>
+              <li>
+                <a href="/spanish/grammar/indefinite-article">indefinite article</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/grammar/nouns">nouns</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/grammar/adjectives">adjectives</a>
+              </li>
+              <li>
+                <a onClick={handleProtectedClick} href="/spanish/grammar/verbs">verbs</a>
+              </li>
+            </ol>
           </div> 
           <div style={{ paddingTop:'20px' }}>
             <b>Phase 4:</b> Spend 1/3rd of time learning <a href="/spanish/vocabulary?s=ver-T0TFT">verbs</a>, 1/3 of time learning <a href="/spanish/vocabulary?s=clo-T0TFT">vocabulary</a>, 1/3 of time doing <a href="/spanish/comprehension/shopping-for-family-meals?eng=F&L=WritingSystem1&R=English">reading and listening comprehension</a>.
