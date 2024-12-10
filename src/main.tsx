9c7f4f7cb71ba539b959ab0974c28d0ffab91109
Kevin Cramer <kevincramer26@gmail.com>
2024-12-10 19:52:45 +0000

    feat: adding meta tags, descriptions etc.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,51 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { store } from './redux-store/store'
+import { Helmet } from 'react-helmet'
+
+// Meta Data Configuration
+const routesMeta = {
+  '/': {
+    title: 'Home Page',
+    meta: [
+      { name: 'description', content: 'Learn foreign languages faster. Start Now.' },
+      { name: 'keywords', content: 'language learning, foreign languages, learn languages, language study tools, fast language learning, online language courses, language practice, multilingual, language learning platform' },
+    ],
+  },
+  '/contact': {
+    title: 'LingoCommand Contact Us',
+    meta: [
+      { name: 'description', content: 'You can reach out to us at contact@lingocommand.com' },
+      { name: 'keywords', content: 'contact us, LingoCommand support, customer support, language learning help, language app support, contact language platform, reach out, LingoCommand inquiries, contact page' },
+    ],
+  },
+};
+
+// Dynamic Helmet Component
+const DynamicMeta = () => {
+  const location = useLocation();
+  const metaInfo = routesMeta[location.pathname as keyof typeof routesMeta] || {};
+
+  return (
+    <Helmet>
+      {metaInfo.title && <title>{metaInfo.title}</title>}
+      {metaInfo.meta &&
+      metaInfo.meta.map((tag: any, index: any) => (
+        <meta key={index} name={tag.name} content={tag.content} />
+      ))}
+    </Helmet>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <DynamicMeta/>
       <Provider store = {store}>
         <AuthProvider>
           <App />
