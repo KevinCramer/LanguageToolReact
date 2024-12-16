c51db262328ee6516610964dda8b9b34807926e3
Kevin Cramer <kevincramer26@gmail.com>
2024-12-16 20:08:52 +0000

    feat: adding video guide for typing japanese

--- src/pages/Japanese/HowToTypeJapanese.tsx
+++ src/pages/Japanese/HowToTypeJapanese.tsx
@@ -1,6 +1,18 @@
-import './Japanese.scss'
+import { Modal } from 'react-bootstrap';
+import './Japanese.scss';
+import React, { useState } from 'react';
+import howToTypeJapaneseTutorial from '../../data/raw-data/tutorial-videos/how-to-type-japanese.mp4'
 
-const HowToTypeJapanese = () => { 
+const HowToTypeJapanese = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
@@ -8,21 +20,46 @@ const HowToTypeJapanese = () => {
         <div className="central-container">
           <h4 style={{ paddingBottom: '30px' }}>How to Type Japanese</h4>
           <div style={{ paddingBottom: '20px' }}>
-            How to Type Japanese on a mac: video coming soon.             
+            <a href="#" onClick={openModal} style={{ color: 'blue', textDecoration: 'underline' }}>
+              How to Type Japanese on a Mac
+            </a>
           </div>
           <div style={{ paddingBottom: '20px' }}>
-              How to Type Japanese on windows: video coming soon.          
+            How to Type Japanese on Windows: video coming soon.
           </div>
           <div style={{ paddingBottom: '20px' }}>
-            How to Type Japanese on a iPhone: video coming soon. 
+            How to Type Japanese on an iPhone: video coming soon.
           </div>
           <div style={{ paddingBottom: '20px' }}>
-            How to Type Japanese on an android: video coming soon.          
+            How to Type Japanese on an Android: video coming soon.
           </div>
         </div>
       </div>
+
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-container">
+            <Modal show={isModalOpen} onHide={() => setIsModalOpen(false)} size="lg" centered>
+              <Modal.Header closeButton>
+                <Modal.Title style={{ textAlign: 'center', width: '100%' }}>How to Guide</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div>
+                  <video
+                    style={{ margin: '0 auto', display: 'block', maxWidth: '100%' }}
+                    controls
+                    src={howToTypeJapaneseTutorial}
+                  />
+                </div>
+              
+              </Modal.Body>
+            </Modal>
+          </div>
+          
+        </div>
+      )}
     </>
   );
-}
- 
+};
+
 export default HowToTypeJapanese;
