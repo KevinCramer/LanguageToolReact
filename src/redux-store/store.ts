07dcc4efe7385839a55458d9c4121ff23bb81f56
Kevin Cramer <kevincramer26@gmail.com>
2025-01-07 12:26:03 +0000

    fix: making appropriate change to redux store.ts

--- src/redux-store/store.ts
+++ src/redux-store/store.ts
@@ -1,11 +1,9 @@
 import authSlice from './auth'
 import { configureStore } from '@reduxjs/toolkit';
 import lockSlice from './lock';
-import navbarSlice from './navbar'
 
 export const store = configureStore({
   reducer: {
-    navbar: navbarSlice,
     auth: authSlice,
     lock: lockSlice,
   },
