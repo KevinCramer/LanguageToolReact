7d52a5898015c02b15177de3055584a4e61175f4
Kevin Cramer <kevincramer26@gmail.com>
2025-01-07 19:30:05 +0000

    feat: styling the contact page

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -7,62 +7,42 @@ import {
   FaYoutube,
 } from 'react-icons/fa';
 
+const socialLinks = [
+  { href: 'https://www.youtube.com/@lingocommand', icon: FaYoutube, label: 'YouTube' },
+  { href: 'https://www.tiktok.com/@lingocommand', icon: FaTiktok, label: 'TikTok' },
+  { href: 'https://www.instagram.com/lingocommand/', icon: FaInstagram, label: 'Instagram' },
+  { href: 'https://www.facebook.com/profile.php?id=100068758832446', icon: FaFacebook, label: 'Facebook' },
+  { href: 'https://www.linkedin.com/company/lingocommand/posts/?feedView=all', icon: FaLinkedin, label: 'LinkedIn' },
+  { href: 'https://pinterest.com/lingocommand/', icon: FaPinterest, label: 'Pinterest' },
+];
+
 const ContactUs = () => {
-  
   return (
-    <>
-      <div>
-        <div>
-          You can reach out to us at&nbsp;
-        </div>
-        <a href="mailto:contact@lingocommand.com">
-          contact@lingocommand.com
-        </a>
-        <div>
-          Social media:
-        </div>
-        <div
-        >
-          <a
-            href="https://www.youtube.com/@lingocommand"
-            
-          >
-            <FaYoutube size={30} />
-            <span >YouTube</span>
-          </a>
-          <a
-            href="https://www.tiktok.com/@lingocommand"
-          >
-            <FaTiktok size={30} />
-            <span >TikTok</span>
-          </a>
-          <a
-            href="https://www.instagram.com/lingocommand/"
-          >
-            <FaInstagram size={30} />
-            <span >Instagram</span>
-          </a>
-          <a
-            href="https://www.facebook.com/profile.php?id=100068758832446"
-          >
-            <FaFacebook size={30} />
-            <span >Facebook</span>
-          </a>
-          <a
-            href="https://www.linkedin.com/company/lingocommand/posts/?feedView=all"
-          >
-            <FaLinkedin size={30} />
-            <span >LinkedIn</span>
-          </a>
-          <a
-            href="https://pinterest.com/lingocommand/"
-          >
-            <FaPinterest size={30} />
-            <span >Pinterest</span>
-          </a>
-        </div>
+    <div className="flex flex-col items-center">
+      <div className="pt-24">
+        You can reach out to us at&nbsp;
       </div>
-    </>
+      <a href="mailto:contact@lingocommand.com" className="text-blue-500">
+        contact@lingocommand.com
+      </a>
+      <div className="py-8">
+        Social media:
+      </div>
+      <div>
+        {socialLinks.map(({ href, icon: Icon, label }) => (
+          <a
+            key={label}
+            href={href}
+            className="flex flex-row items-center p-4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon size={30} />
+            <span className="pl-2">{label}</span>
+          </a>
+        ))}
+      </div>
+    </div>
   );
 };
 
