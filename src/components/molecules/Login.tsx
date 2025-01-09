b16dcc56d676fc994b925af5840b5b20c35f28eb
Kevin Cramer <kevincramer26@gmail.com>
2025-01-09 17:18:26 +0000

    feat: improving tailwind styling on login page

--- src/components/molecules/Login.tsx
+++ src/components/molecules/Login.tsx
@@ -37,29 +37,27 @@ export default function Login() {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="relative bg-white p-6 rounded-lg shadow-lg w-full h-full">
-        <div className="flex justify-end pb-2">
-          <button
-            onClick={() => dispatch(hideModal())}
-            aria-label="Close">
-            <svg
-              className="h-6 w-6 fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M19.707 4.293a1 1 0 010 1.414L13.414 12l6.293 6.293a1 1 0 01-1.414 1.414L12 13.414l-6.293 6.293a1 1 0 01-1.414-1.414L10.586
+      <div className="relative bg-white p-6  shadow-lg w-full h-full flex items-center justify-center">
+        <div className="max-w-screen-md mx-auto px-4 md:text-lg">
+          <div className="flex justify-end pb-2">
+            <button
+              onClick={() => dispatch(hideModal())}
+              aria-label="Close">
+              <svg
+                className="h-6 w-6 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M19.707 4.293a1 1 0 010 1.414L13.414 12l6.293 6.293a1 1 0 01-1.414 1.414L12 13.414l-6.293 6.293a1 1 0 01-1.414-1.414L10.586
                     12 4.293 5.707a1 1 0 011.414-1.414L12 10.586l6.293-6.293a1 1 0 011.414 0z"
-              />
-            </svg>
-          </button>
-        </div>
-        {/* need to fix the pt-[30vh that is just a hack there should be a way to center the login modal in the center of the screen]*/}
-        <div className='max-w-screen-md mx-auto px-4 md:text-lg'>
-         
-          <h4 className="text-2xl mb-4 text-center p-4">Welcome Back.</h4>
-          <div className="mt-4 flex flex-row justify-center">
+                />
+              </svg>
+            </button>
+          </div>
+          <h4 className="text-2xl mb-4 text-center ">Welcome Back.</h4>
+          <div className="mt-4 flex flex-row justify-center p-4">
             <div>New to LingoCommand? &nbsp;</div>
             <CustomLink onClick={() => dispatch(displaySignup())}>Create account</CustomLink>
           </div>
@@ -67,7 +65,7 @@ export default function Login() {
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label htmlFor="email" className="block text-sm font-medium">
-                Email
+              Email
               </label>
               <input
                 type="email"
@@ -75,13 +73,13 @@ export default function Login() {
                 ref={emailRef}
                 required
                 className="mt-1 block w-full p-2 border
-                border-gray-300 rounded-md shadow-sm
-                focus:ring-indigo-500 focus:border-indigo-500"
+              border-gray-300 rounded-md shadow-sm
+              focus:ring-indigo-500 focus:border-indigo-500"
               />
             </div>
             <div className="mb-4">
               <label htmlFor="password" className="block text-sm font-medium">
-                Password
+              Password
               </label>
               <input
                 type="password"
@@ -89,27 +87,27 @@ export default function Login() {
                 ref={passwordRef}
                 required
                 className="mt-1 block w-full p-2 border
-                border-gray-300 rounded-md shadow-sm
-                focus:ring-indigo-500 focus:border-indigo-500"
+              border-gray-300 rounded-md shadow-sm
+              focus:ring-indigo-500 focus:border-indigo-500"
               />
             </div>
             <button
               type="submit"
               disabled={loading}
               className="w-full bg-blue-500 text-white py-2
-              px-4 rounded hover:bg-blue-600 disabled:opacity-50"
+            px-4 rounded hover:bg-blue-600 disabled:opacity-50"
             >
-              Log In
+            Log In
             </button>
           </form>
           <div className="mt-4 text-center">
             <CustomLink onClick={() => dispatch(displayForgotPassword())}>
-              Forgot Password?
+            Forgot Password?
             </CustomLink>
           </div>
         </div>
-       
       </div>
     </div>
+  
   );
 }
