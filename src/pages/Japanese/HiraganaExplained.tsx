bdf37c53fc5aa3c04d6fa0ecbf869f2e0067f302
Kevin Cramer <kevincramer26@gmail.com>
2025-01-10 20:13:49 +0000

    feat: getting it to be like a toggle menu

--- src/pages/Japanese/HiraganaExplained.tsx
+++ src/pages/Japanese/HiraganaExplained.tsx
@@ -1,27 +1,31 @@
-import { japaneseWritingSystemsTopicSlugNames, writingSystems }
-  from '../../data/structured-data/writingSystems';
+import { useState } from 'react';
+import { japaneseWritingSystemsTopicSlugNames, writingSystems } from '../../data/structured-data/writingSystems';
 import { LanguageNames, LearningSections } from '../../../types/LearningSectionsTypes';
 import { createURL } from '../../helpers/createURL';
 import { lingoCommandIsLocked } from '../../constants';
 import LockIcon from '@mui/icons-material/Lock';
 import { useAuth } from '../../contexts/AuthContext';
 import { useProtectedLink } from '../../helpers/use-protected-link';
-import { useState } from 'react';
 
-const HiraganaExplained = () => { 
+const HiraganaExplained = () => {
   //@ts-ignore
   const { currentUser } = useAuth();
-  const userIsLoggedIn = currentUser && currentUser.email
+  const userIsLoggedIn = currentUser && currentUser.email;
   const handleProtectedClick = useProtectedLink();
 
-  const japaneseWritingSystems = writingSystems[0]
+  const japaneseWritingSystems = writingSystems[0];
 
-  const hiraganaBasicWritingSystem = japaneseWritingSystems?.topics
-    .find((t: any)=> t.slugName === japaneseWritingSystemsTopicSlugNames.basicHiragana)
-  const hiraganaDakutenWritingSystem = japaneseWritingSystems?.topics
-    .find((t: any)=> t.slugName === japaneseWritingSystemsTopicSlugNames.hiraganaDakuten)
-  const hiraganaYoonWritingSystem = japaneseWritingSystems?.topics
-    .find((t: any)=> t.slugName === japaneseWritingSystemsTopicSlugNames.hiraganaYoon)
+  const hiraganaBasicWritingSystem = japaneseWritingSystems?.topics.find(
+    (t: any) => t.slugName === japaneseWritingSystemsTopicSlugNames.basicHiragana
+  );
+  const hiraganaDakutenWritingSystem = japaneseWritingSystems?.topics.find(
+    (t: any) => t.slugName === japaneseWritingSystemsTopicSlugNames.hiraganaDakuten
+  );
+  const hiraganaYoonWritingSystem = japaneseWritingSystems?.topics.find(
+    (t: any) => t.slugName === japaneseWritingSystemsTopicSlugNames.hiraganaYoon
+  );
+
+  const [currentLesson, setCurrentLesson] = useState(1); // Track the active lesson
 
   const dakutenTable = <table>
     <thead>
@@ -168,124 +172,114 @@ const HiraganaExplained = () => {
     setBasicHiraganaModalOpen(!isBasicHiraganaModalOpen);
   };
 
-  return (
-    <div className='max-w-screen-md mx-auto px-4 md:text-lg'>
-      <h4 className='text-center text-2xl py-12'>Hiragana Explained</h4>
-      <div className='py-2'>
-          Learning hiragana is the first step to learning japanese.
-          By learning all three lessons in order you will learn the basics of Japanese pronunciation. 
-      </div>
-      <div className='py-2'>
-        <div>
-          <b className='underline'>Lesson 1: </b>
-        </div>
-        Basic hiragana  has 46 sounds. See this table for the basic <span className='text-blue-500 underline'
-          onClick={toggleBasicHiraganaModal}>
-          pattern
-        </span>
-        {isBasicHiraganaModalOpen && (
+  const LessonContent = () => {
+    switch (currentLesson) {
+    case 1:
+      return (
+        <div className="py-2">
           <div>
+            <b className="underline">Lesson 1: </b>
+          </div>
+            Basic hiragana has 46 sounds. See this table for the basic{' '}
+          <span className="text-blue-500 underline" onClick={toggleBasicHiraganaModal}>
+              pattern
+          </span>
+          {isBasicHiraganaModalOpen && (
             <div>
               <div>
                 <h3>Hiragana Chart</h3>
-                <button 
-                  onClick={toggleBasicHiraganaModal}>
-                  Close
-                </button>
+                <button onClick={toggleBasicHiraganaModal}>Close</button>
               </div>
-             
+              {basicHiraganaTable}
             </div>
-            {basicHiraganaTable}                  
+          )}
+          <div className="flex flex-col space-y-2 justify-center items-center m-8">
+            <a
+              className="text-black border text-center border-black bg-gray-200 active:bg-gray-50 hover:bg-gray-400 p-2 rounded-md"
+              onClick={handleProtectedClick(hiraganaBasicWritingSystem)}
+              href={createURL(LanguageNames.Japanese, LearningSections.WritingSystem, hiraganaBasicWritingSystem)}
+            >
+                Basic Hiragana Exercise
+            </a>
           </div>
-        )}
-
-        {isBasicHiraganaModalOpen && (
-          <div
-            onClick={toggleBasicHiraganaModal}
-          />
-        )}
-      </div>
-      <div className='flex flex-col space-y-2 justify-center items-center m-8'>
-        <a className='text-black border text-center border-black bg-gray-200 active:bg-gray-50 hover:bg-gray-400 border-black p-2 rounded-md'
-          onClick={handleProtectedClick(hiraganaBasicWritingSystem)}
-          href={
-            createURL(
-              LanguageNames.Japanese,
-              LearningSections.WritingSystem,
-              hiraganaBasicWritingSystem)
-          }>
-            Basic Hiragana Exercise</a> 
-      </div>
-      
-      <div className='py-2'>
-        <div>
-          <b className='underline'>Lesson 2: </b>
         </div>
-        <div>
+      );
+    case 2:
+      return (
+        <>
+          <div className='py-2'>
+            <div>
+              <b className='underline'>Lesson 2: </b>
+            </div>
+            <div>
           Dakuten and Handakuten lead to another 25 hiragana variations. 
-        </div>
-        <br></br>
-        <div>
+            </div>
+            <br></br>
+            <div>
           Dakuten and handakuten are marks added on top of hiragana that alter pronunciation. 
-        </div>
-        <div>
-        </div>
+            </div>
+            <div>
+            </div>
             The dakuten mark is <b>( ゛)</b>. There are 20 dakuten variations.
             For example: <b>か (ka) → が (ga)</b>. 
-        <div>
-        </div>
+            <div>
+            </div>
             The handakuten mark is <b>( ゜)</b>. There are 5 variations.
             For example: <b>は (ha) → ぱ (pa)</b>.
-        <div>
-          <i>
-            See this useful <span className='text-blue-500 underline'
-              onClick={toggleDakutenModal}>
-              diagram
-            </span> for the general pattern.
-          </i>
-          {isDakutenModalOpen && (
             <div>
-              <div>
-                <button onClick={toggleDakutenModal}>
+              <i>
+            See this useful <span className='text-blue-500 underline'
+                  onClick={toggleDakutenModal}>
+              diagram
+                </span> for the general pattern.
+              </i>
+              {isDakutenModalOpen && (
+                <div>
+                  <div>
+                    <button onClick={toggleDakutenModal}>
                   Close
-                </button>
-              </div>
-              {dakutenTable}
-              <div>
+                    </button>
+                  </div>
+                  {dakutenTable}
+                  <div>
                     (1) ぢ is written in romaji as <b>di</b> but is pronounced <b>ji</b>.
-              </div>
-              <div>
+                  </div>
+                  <div>
                     (2) づ is written in romaji as <b>du</b> but is pronounced <b>zu</b>.
-              </div>       
+                  </div>       
+                </div>
+              )}
+              {isDakutenModalOpen && (
+                <div
+                  onClick={toggleDakutenModal}
+                />
+              )}
             </div>
-          )}
-          {isDakutenModalOpen && (
-            <div
-              onClick={toggleDakutenModal}
-            />
-          )}
-        </div>
-        <div>
-        </div>      
-      </div>
-      <div className='flex flex-col space-y-2 justify-center items-center m-8'>
-        <a className='text-black border text-center border-black bg-gray-200 active:bg-gray-50 hover:bg-gray-400 border-black p-2 rounded-md'
-          onClick={handleProtectedClick(hiraganaDakutenWritingSystem)} 
-          href={
-            createURL(
-              LanguageNames.Japanese,
-              LearningSections.WritingSystem,
-              hiraganaDakutenWritingSystem)
-          }> Hiragana with Dakuten and Handakuten Exercise
-          {hiraganaDakutenWritingSystem?.isLocked &&
-                   lingoCommandIsLocked && !userIsLoggedIn ? <LockIcon/> : ''}
-        </a>
-      </div>
-     
-      <div className='py-2'>
-        <div>
-          <b className='underline'>Lesson 3: </b>
-        </div>
+            <div>
+            </div>      
+          </div>
+          <div className='flex flex-col space-y-2 justify-center items-center m-8'>
+            <a className='text-black border text-center border-black bg-gray-200 active:bg-gray-50 hover:bg-gray-400 border-black p-2 rounded-md'
+              onClick={handleProtectedClick(hiraganaDakutenWritingSystem)} 
+              href={
+                createURL(
+                  LanguageNames.Japanese,
+                  LearningSections.WritingSystem,
+                  hiraganaDakutenWritingSystem)
+              }> Hiragana with Dakuten and Handakuten Exercise
+              {hiraganaDakutenWritingSystem?.isLocked &&
+                    lingoCommandIsLocked && !userIsLoggedIn ? <LockIcon/> : ''}
+            </a>
+          </div>
+        </>
+      );
+    case 3:
+      return (
+        <div className="py-2">
+          <div className='py-2'>
+            <div>
+              <b className='underline'>Lesson 3: </b>
+            </div>
         Yōon  leads to an additional 36 hiragana variations.
             Some hiragana combine to form Yōon sounds. In these combinations,
               the first character remains full-sized, while the second is smaller.
@@ -293,26 +287,63 @@ const HiraganaExplained = () => {
               syllables rather than separate sounds. This blending is crucial in words 
               like <br></br><b>きょう(kyou, today)</b> and <b>
               しょうがっこう(shougakkou, elementary school)</b>. 
-      </div>
-      <div className='flex flex-col space-y-2 justify-center items-center m-8'>
+          </div>
+          <div className='flex flex-col space-y-2 justify-center items-center m-8'>
    
-        <a className='text-black border text-centers border-black bg-gray-200 active:bg-gray-50 hover:bg-gray-400 border-black p-2 rounded-md'
-          onClick={handleProtectedClick(hiraganaYoonWritingSystem)} 
-          href={
-            createURL(
-              LanguageNames.Japanese,
-              LearningSections.WritingSystem,
-              hiraganaYoonWritingSystem)
-          }>
+            <a className='text-black border text-centers border-black bg-gray-200 active:bg-gray-50 hover:bg-gray-400 border-black p-2 rounded-md'
+              onClick={handleProtectedClick(hiraganaYoonWritingSystem)} 
+              href={
+                createURL(
+                  LanguageNames.Japanese,
+                  LearningSections.WritingSystem,
+                  hiraganaYoonWritingSystem)
+              }>
                Study Hiragana Yoōn combinations
-          {hiraganaYoonWritingSystem?.isLocked &&
+              {hiraganaYoonWritingSystem?.isLocked &&
                    lingoCommandIsLocked && !userIsLoggedIn ? <LockIcon/> : ''}
-        </a>
+            </a>
+          </div>
+        </div>
+      );
+    default:
+      return null;
+    }
+  };
+
+  return (
+    <div className="max-w-screen-md mx-auto px-4 md:text-lg">
+      <h4 className="text-center text-2xl py-12">Hiragana Explained</h4>
+      <div className="py-2">
+        Learning hiragana is the first step to learning Japanese. By learning all three lessons in order, you will learn
+        the basics of Japanese pronunciation.
       </div>
-    
+
+      {/* Display Lesson Content */}
+      <LessonContent />
+
+      {/* Navigation */}
+      <div className="flex justify-center items-center space-x-4 mt-8">
+        <button
+          className={`px-4 py-2 border rounded-md ${currentLesson === 1 ? 'bg-gray-300' : 'bg-gray-100'}`}
+          onClick={() => setCurrentLesson(1)}
+        >
+          1
+        </button>
+        <button
+          className={`px-4 py-2 border rounded-md ${currentLesson === 2 ? 'bg-gray-300' : 'bg-gray-100'}`}
+          onClick={() => setCurrentLesson(2)}
+        >
+          2
+        </button>
+        <button
+          className={`px-4 py-2 border rounded-md ${currentLesson === 3 ? 'bg-gray-300' : 'bg-gray-100'}`}
+          onClick={() => setCurrentLesson(3)}
+        >
+          3
+        </button>
+      </div>
     </div>
-    
   );
-}
- 
+};
+
 export default HiraganaExplained;
