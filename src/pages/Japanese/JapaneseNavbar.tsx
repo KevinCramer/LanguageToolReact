bec453b140a09d262117e181b669266c397583b3
Kevin Cramer <kevincramer26@gmail.com>
2025-01-10 21:32:53 +0000

    feat: ensuring japaneseNavbar works for mobile as well as larger screen size

--- src/pages/Japanese/JapaneseNavbar.tsx
+++ src/pages/Japanese/JapaneseNavbar.tsx
@@ -2,24 +2,42 @@ import { NavLink } from 'react-router-dom';
 
 const JapaneseNavbar = () => {
   return (
-    <nav className='flex justify-around p-4 bg-gray-500 text-white'>
-      <NavLink to='/japanese'>
-          Japanese Home Page
-      </NavLink>
-      <NavLink to='/japanese/study-guide'>
-          Study Guide
-      </NavLink>
-      <NavLink to='/japanese/writing-systems-explained'>
-          Writing Systems
-      </NavLink>
-      <NavLink to='/japanese/vocabulary'>
-          Vocabulary
-      </NavLink>
-      <NavLink to='/japanese/grammar/keigo'>
-          Grammar
-      </NavLink> <NavLink to='/japanese/comprehension/aikos-book-sanctuary'>
-          Reading/Listening
-      </NavLink>
+    <nav className="bg-gray-500 text-white p-4">
+      {/* Add a wrapper div with horizontal overflow */}
+      <div className="overflow-x-auto whitespace-nowrap">
+        <ul className="flex space-x-4 justify-start lg:justify-around">
+          <li>
+            <NavLink to='/japanese' className="block px-4 py-2 hover:bg-gray-700">
+              Japanese Home Page
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/japanese/study-guide' className="block px-4 py-2 hover:bg-gray-700">
+              Study Guide
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/japanese/writing-systems-explained' className="block px-4 py-2 hover:bg-gray-700">
+              Writing Systems
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/japanese/vocabulary' className="block px-4 py-2 hover:bg-gray-700">
+              Vocabulary
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/japanese/grammar/keigo' className="block px-4 py-2 hover:bg-gray-700">
+              Grammar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/japanese/comprehension/aikos-book-sanctuary' className="block px-4 py-2 hover:bg-gray-700">
+              Reading/Listening
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
