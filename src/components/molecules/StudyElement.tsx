c45c55aabb8a3bc678e621160b2047ce77cabf6f
Kevin Cramer <kevincramer26@gmail.com>
2025-01-11 12:52:37 +0000

    feat: getting stroke order videos working on writing systems pages

--- src/components/molecules/StudyElement.tsx
+++ src/components/molecules/StudyElement.tsx
@@ -1,4 +1,4 @@
-import { Modal, Navbar } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
 import CustomButton from '../atoms/CustomButton'
 import { useEffect, useState } from 'react'
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
@@ -65,7 +65,7 @@ const StudyElement = (
     <>{BaseLanguageWord}</>
   </label>;
 
-  const foreignLanguageLabelStrokeOrder = <label
+  const foreignLanguageLabelStrokeOrder = <label className='text-blue-500 underline'
     onClick={showBaseLanguageFirst ? displayPopUp : () => {}}>
     <> {ForeignLanguageWord}</>
   </label>;
@@ -89,21 +89,44 @@ const StudyElement = (
         </div>
       )}
       {strokeOrderVideo && (
-        <Modal show={showPopUp} onHide={hidePopUp} size='lg' centered>
-          <Modal.Header closeButton>
-            <Modal.Title>
-              {ForeignLanguageWord.toString() + ' - Hiragana Stroke Order'}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div>
-              <video width='100%' controls>
-                <source src={strokeOrderVideo} type='video/mp4'/>
-                Your browser does not support the video tag.
-              </video>
+        <>
+          {showPopUp && (
+            <div className="fixed inset-0 flex justify-center items-center z-50 bg-gray-900 bg-opacity-50">
+              <div className="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full">
+                <div className="flex justify-between items-center mb-4">
+                  <h3 className="text-xl font-semibold">
+                    {ForeignLanguageWord} - Hiragana Stroke Order
+                  </h3>
+                  <button
+                    onClick={hidePopUp}
+                    className="text-gray-500 hover:text-gray-700 focus:outline-none"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <div>
+                  <video className="w-full" controls>
+                    <source src={strokeOrderVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+              </div>
             </div>
-          </Modal.Body>
-        </Modal>
+          )}
+        </>
       )}
     </Navbar>
   );
