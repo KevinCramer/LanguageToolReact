3ad16d4582dde11a669ce70a8686d05e3b25cc09
Kevin Cramer <kevincramer26@gmail.com>
2025-01-13 23:36:13 +0000

    feat: fixing padding on grammar page

--- src/pages/GrammarContent.tsx
+++ src/pages/GrammarContent.tsx
@@ -71,7 +71,7 @@ const GrammarContent = (props: { languageNumber: number }) => {
   }, []);
 
   return (
-    <div className="max-w-screen-md mx-auto md:text-lg">
+    <div className="max-w-screen-md mx-auto px-4 md:text-lg">
       <h4 className="text-center text-2xl py-12">
         {languages[props.languageNumber].languageName} Grammar
       </h4>
