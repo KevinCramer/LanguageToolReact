b8b2a68fc503524eeff18c72f19f4263cceeb91b
Kevin Cramer <kevincramer26@gmail.com>
2025-01-13 18:56:44 +0000

    dropdowns are centered

--- src/pages/ReadingListening.tsx
+++ src/pages/ReadingListening.tsx
@@ -143,14 +143,14 @@ const ReadingListeningContent = (props: { languageNumber: number; howToGuideVide
         transcriptionType !== TranscriptionType.Audio &&
         transcriptionType !== TranscriptionType.English &&
         parseInt(transcriptionType.slice(-1)) > currentLanguage.numForeignAlphabets;
+
       if (showCondition) return null;
 
       return (
         <li
           key={key}
           onClick={() => changeHandler(transcriptionType)}
-          className="cursor-pointer hover:bg-gray-200 p-2"
-        >
+          className='px-4 py-2 text-sm text-gray-800 cursor-pointer hover:bg-gray-200' >
           {title}
         </li>
       );
@@ -202,13 +202,11 @@ const ReadingListeningContent = (props: { languageNumber: number; howToGuideVide
 
                     {(side === 'Left' ? isLeftDropdownOpen : isRightDropdownOpen) && (
                       <div ref={side === 'Left' ? leftDropdownRef : rightDropdownRef}
-                        className="absolute left-0 mt-2 bg-white border
-                         border-gray-300 rounded-lg shadow w-64 z-10">
-                        <ul className="divide-y divide-gray-200">
+                        className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white border border-gray-300 rounded-lg shadow w-44 md:w-64 z-10">                        <ul className="divide-y divide-gray-200">
                           {renderDropdownItems(
                     (side === 'Left' ? currentLeft : currentRight) as TranscriptionType,
                     side === 'Left' ? setCurrentLeft : setCurrentRight
-                          )}
+                          ) }
                         </ul>
                       </div>
                     )}
