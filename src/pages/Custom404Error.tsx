7408fbc3865538a098d089e5f439a491e42e56db
Kevin Cramer <kevincramer26@gmail.com>
2025-01-13 20:08:27 +0000

    fix: fixing 404 error page

--- src/pages/Custom404Error.tsx
+++ src/pages/Custom404Error.tsx
@@ -1,6 +1,10 @@
 const Custom404Error = () => { 
   return (
-    <div className='flex flex-col flex-1 justify-center  px-4'>
+    <div className='flex flex-col flex-1 justify-center px-4'>
+      {/* silly hack to get text roughly in the middle, rewrite this properly! */}
+      <div className='h-[150px]'>
+
+      </div>
       <div className='max-w-screen-md mx-auto  md:text-lg rounded-md'>
         Oh no! That page does not exist.
       </div>   
