b4884247a7de7202947821fda9c5e64ac2a5577a
Kevin Cramer <kevincramer26@gmail.com>
2025-01-13 19:18:18 +0000

    final fix

--- src/components/atoms/PremiumContent.tsx
+++ src/components/atoms/PremiumContent.tsx
@@ -9,7 +9,8 @@ export default function PremiumContent() {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="relative bg-white p-6 shadow-lg w-full h-full max-w-full flex items-center justify-center rounded-lg z-60">
-        <div className="px-4 md:text-lg w-full">
+        <div className="max-w-[375px] mx-auto px-8 md:text-lg w-full">
+          {/* Close Button Positioning */}
           <div className="flex justify-end pb-2">
             <button
               onClick={() => dispatch(resetPermission())} // Close the modal without navigating to login page
@@ -31,23 +32,24 @@ export default function PremiumContent() {
           </div>
 
           <h2 className="text-2xl font-semibold text-center mb-4">Premium Content</h2>
-          <div className="text-center text-gray-700 mb-6">
-            Please{' '}
+          <div className="flex text-center text-gray-700 mb-6">
+            <div>
+            To access this content please &nbsp;
+            </div>
             <CustomLink
               inline={true}
               onClick={() => {
-                dispatch(displayLogin() ); // Close modal on "log in" click
+                dispatch(displayLogin()); // Close modal on "log in" click
                 dispatch(resetPermission());
-              }} >
+              }}
+            >
               log in
-            </CustomLink>{' '}
-            to access this content.
+            </CustomLink>.
           </div>
 
           {/* Conditional Content Based on Authentication */}
           <div className="text-center mt-6">
             {/* Add any additional premium content description or image here */}
-            <p className="text-gray-500">Unlock exclusive content by logging in.</p>
           </div>
         </div>
       </div>
