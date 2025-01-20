a00215906aac91927dfcd7c40ae7f9249c7754e5
Kevin Cramer <kevincramer26@gmail.com>
2025-01-20 19:37:50 +0000

    removing facebook and pinterest

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,8 +1,6 @@
 import {
-  FaFacebook,
   FaInstagram,
   FaLinkedin,
-  FaPinterest,
   FaTiktok,
   FaYoutube,
 } from 'react-icons/fa';
@@ -11,9 +9,7 @@ const socialLinks = [
   { href: 'https://www.youtube.com/@lingocommand', icon: FaYoutube, label: 'YouTube' },
   { href: 'https://www.tiktok.com/@lingocommand', icon: FaTiktok, label: 'TikTok' },
   { href: 'https://www.instagram.com/lingocommand/', icon: FaInstagram, label: 'Instagram' },
-  { href: 'https://www.facebook.com/profile.php?id=100068758832446', icon: FaFacebook, label: 'Facebook' },
   { href: 'https://www.linkedin.com/company/lingocommand/posts/?feedView=all', icon: FaLinkedin, label: 'LinkedIn' },
-  { href: 'https://pinterest.com/lingocommand/', icon: FaPinterest, label: 'Pinterest' },
 ];
 
 const ContactUs = () => {
