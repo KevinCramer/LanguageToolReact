5ac205dfc146011a2b7e7a5aa277b250a7530f03
Kevin Cramer <kevincramer26@gmail.com>
2025-01-25 12:37:35 +0000

    fix: making timeElapsed and timeRemaining timestamps synced up

--- src/helpers/audio-player-helpers.ts
+++ src/helpers/audio-player-helpers.ts
@@ -4,13 +4,15 @@ export function nullOrUndefined(value: string | boolean | number): boolean {
 
 const secondsInMinute = 60;
 export function timeElapsed(currentSeconds: number): string {
+  // assumes audio files are less than 1 hour
   return `${Math.floor(currentSeconds / secondsInMinute)}:${(Math.floor(currentSeconds) - 
     secondsInMinute * Math.floor(currentSeconds / secondsInMinute)).toString().padStart(2, '0')}`
 
 }
 
 export function timeRemaining(currentSeconds: number, totalSeconds: number): string {
-  const secondsRemaining = Math.max(totalSeconds - currentSeconds, 0); // Prevent negative values
+  const secondsRemaining = Math.max(Math.ceil(totalSeconds) - currentSeconds, 0); // Prevent negative values
+  // assumes audio files are less than 1 hour
   return `${Math.floor((secondsRemaining) / secondsInMinute)}:${
     (Math.floor(secondsRemaining) - secondsInMinute *
      Math.floor((secondsRemaining) / secondsInMinute))
