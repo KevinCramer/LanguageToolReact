69894429d004de4a355d82417706518a03420c21
Kevin Cramer <kevincramer26@gmail.com>
2025-01-28 08:50:11 +0000

    refactor: removing linting warnings on this writing systems explained

--- src/pages/Japanese/WritingSystemsExplained.tsx
+++ src/pages/Japanese/WritingSystemsExplained.tsx
@@ -4,16 +4,19 @@ const JapaneseWritingSystems = () => {
   const sections = [
     {
       title: 'Hiragana',
+      // eslint-disable-next-line @stylistic/js/max-len
       description: 'a phonetic writing system used for native Japanese words. (It has 46 basic characters)',
       link: '/japanese/hiragana-explained',
     },
     {
       title: 'Katakana',
+      // eslint-disable-next-line @stylistic/js/max-len
       description: 'a phonetic writing system mainly used for loan words from other languages. (It has 46 basic characters)',
       link: '/japanese/katakana-explained',
     },
     {
       title: 'Kanji',
+      // eslint-disable-next-line @stylistic/js/max-len
       description: 'a non-phonetic writing system. (It has over 50,000 characters). The Japanese government has designated 2,136 characters as \'Jōyō kanji\' (\'daily use kanji\'). High school students are expected to be able to read and write these kanji by the time they graduate.',
       link: '/japanese/kanji-explained',
     },
@@ -21,13 +24,18 @@ const JapaneseWritingSystems = () => {
 
   const romajiSection = (
     <div className='pt-4 pb-2'>
-      Finally, <b>Romaji (ローマ字)</b> is the Romanized version of the Japanese writing system. It uses the Latin alphabet to represent Japanese sounds, making it easier for people who are not familiar with Japanese characters (like kanji, hiragana, or katakana) to read and pronounce Japanese words.
+      Finally, <b>Romaji (ローマ字)</b> is the Romanized version of the Japanese writing system.
+      It uses the Latin alphabet to represent Japanese sounds, making it easier for people who
+      are not familiar with Japanese characters (like kanji, hiragana, or katakana) to read and
+      pronounce Japanese words.
     </div>
   );
 
   const typingJapaneseSection = (
     <div className='py-2'>
-      <a className="text-blue-500 underline" href='/japanese/how-to-type-japanese'>How to Type Japanese</a>
+      <a className="text-blue-500 underline" href='/japanese/how-to-type-japanese'>
+        How to Type Japanese
+      </a>
     </div>
   );
 
@@ -37,7 +45,10 @@ const JapaneseWritingSystems = () => {
       <div className='py-2'>The Japanese language has 3 writing systems:</div>
       {sections.map((section, index) => (
         <div className='py-2' key={index}>
-          <a className='text-blue-500 underline' href={section.link}>{section.title}</a> - {section.description}
+          <a className='text-blue-500 underline' href={section.link}>
+            {section.title}
+          </a>
+           &nbsp;- {section.description}
         </div>
       ))}
       {romajiSection}
