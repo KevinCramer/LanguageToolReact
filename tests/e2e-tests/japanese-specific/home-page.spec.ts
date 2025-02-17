87347b5d2d590475bc67ee41d549b697ae74278a
Kevin Cramer <kevincramer26@gmail.com>
2025-02-17 02:46:07 +0200

    test: improving e2e test template for home-page

--- tests/e2e-tests/japanese-specific/home-page.spec.ts
+++ tests/e2e-tests/japanese-specific/home-page.spec.ts
@@ -1,26 +1,74 @@
-/* eslint-disable @stylistic/js/max-len */
 
 // ----- MOBILE -----
 
-// 1. LingoCommand logo in top left corner with fontStretch 50%
-// 2. 'Japanese', 'About', and 'Login' are present in navbar. 'Contact' is not present in navbar.
-// 3. 'Login' in navbar has normal font weight. 
-// 4. 'Learn Japanese Faster' text is present in middle of the screen with 0.25 rem letter spacing
-// 5. Blue 'Start Now' button is present in the middle of the screen below 'Learn Japanese Faster' with 0.25 rem letter spacing
-// 6. Mount Fuji picture is the background
-// 7. If user clicks on 'Japanese' in navbar then gets redirected to '/japanese/home-page'
-// 8. If user clicks on 'About' in navbar then gets redirected to '/japanese/about'
-// 9. If user clicks on 'Login' in navbar then gets redirected to '/login'
+// 1.
+// LingoCommand logo in top left corner with fontStretch 50%
+
+// 2.
+// 'Japanese', 'About', and 'Login' are present in navbar. None are underlined.
+
+// 3.
+// 'Contact' is not present in navbar.
+
+// 4.
+// 'Login' in navbar has normal font weight. 
+
+// 5.
+// 'Learn Japanese Faster' text is present in middle of the screen with 0.25 rem
+// letter spacing
+
+// 6.
+// Blue 'Start Now' button is present in the middle of the screen below 
+// 'Learn Japanese Faster' with 0.25 rem letter spacing. Also if you press 
+// the button then gets redirected to '/japanese/home-page'
+
+// 7.
+// Mount Fuji picture is the background
+
+// 8.
+// If user clicks on 'LingoCommand' logo in navbar then users stays on '/'
+
+// 9.
+// If user clicks on 'Japanese' in navbar then gets redirected to '/japanese/home-page'
+
+// 10.
+// If user clicks on 'About' in navbar then gets redirected to '/japanese/about'
+
+// 11.
+// If user clicks on 'Login' in navbar then gets redirected to '/login'
 
 // ----- DESKTOP -----
 
-// 1. LingoCommand logo in top left corner with fontStretch 100%
-// 2. 'Japanese', 'About', 'Contact', and 'Login' are present in navbar.
-// 3. 'Login' in navbar has bold font weight. 
-// 4. 'Learn Japanese Faster' text is present in middle of the screen with 0.25 rem letter spacing
-// 5. Blue 'Start Now' button is present in the middle of the screen below 'Learn Japanese Faster' with 0.25 rem letter spacing
-// 6. Mount Fuji picture is the background
-// 7. If user clicks on 'Japanese' in navbar then gets redirected to '/japanese/home-page'
-// 8. If user clicks on 'About' in navbar then gets redirected to '/japanese/about'
-// 9. If user clicks on 'Login' in navbar then gets redirected to '/login'
+// 1.
+// LingoCommand logo in top left corner with fontStretch 100%
+
+// 2
+// 'Japanese', 'About', 'Contact', and 'Login' are present in navbar.
+// None are underlined.
+
+// 3.
+// 'Login' in navbar has bold font weight. 
+
+// 4.
+// 'Learn Japanese Faster' text is present in middle of the screen with 0.25 rem letter spacing
+
+// 5.
+// Blue 'Start Now' button is present in the middle of the screen below 
+// 'Learn Japanese Faster' with 0.25 rem letter spacing. Also if you press 
+// the button then gets redirected to '/japanese/home-page'
+
+// 6.
+// Mount Fuji picture is the background
+
+// 7.
+// If user clicks on 'LingoCommand' logo in navbar then users stays on '/'
+
+// 8.
+// If user clicks on 'Japanese' in navbar then gets redirected to '/japanese/home-page'
+
+// 9.
+// If user clicks on 'About' in navbar then gets redirected to '/japanese/about'
+
+// 10.
+// If user clicks on 'Login' in navbar then gets redirected to '/login'
 
